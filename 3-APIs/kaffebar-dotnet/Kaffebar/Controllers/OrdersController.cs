bd9f88095c8a5f9262555013256935766d9e5be6
Beate Biribakken <beatebiribakken@gmail.com>
2026-05-27 17:48:50 +0200

    Oppgave 5 og 6

--- 3-APIs/kaffebar-dotnet/Kaffebar/Controllers/OrdersController.cs
+++ 3-APIs/kaffebar-dotnet/Kaffebar/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
 
 namespace Kaffebar.Controllers
 {
@@ -9,14 +10,30 @@ namespace Kaffebar.Controllers
 
     public class OrdersController : ControllerBase
     {
+        private static readonly ConcurrentDictionary<Guid, CreateOrderResponse> Orders = new();
+
         [HttpPost]
 
         public IActionResult CreateOrder(CreateOrderRequest request)
         {
             var orderId = Guid.NewGuid();
             var created = new CreateOrderResponse(orderId, request.CoffeeId, DateTime.UtcNow);
+            Orders[orderId] = created;
             var location = $"/orders/{orderId}";
             return Created(location, created);
+        }
+
+        [HttpGet("{orderId:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IResult GetOrder(Guid orderId)
+        {
+            if (Orders.TryGetValue(orderId, out var order))
+            {
+                return Results.Ok(order);
+            } else {
+                return Results.Problem("Order not found", statusCode: StatusCodes.Status404NotFound);
+            }
         }
     }
 }
